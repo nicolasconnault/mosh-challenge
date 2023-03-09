5064863ed63c2eb43c746e667e05302f8deb5c4a
nicolasconnault <nicolasconnault@gmail.com>
2023-03-09 22:16:33 +0800

    Using SASS instead of plain CSS

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,16 +1,30 @@
-import { FunctionComponent } from 'react'
-import styles from '@/styles/Card.module.css'
+import Image, { StaticImageData } from 'next/image'
+import styles from '@/styles/Card.module.scss'
 
-const Card: FunctionComponent = ({
-                                       children,
-                                   }: {
-    children: ReactChildren
-}) => {
-    return (
-        <div className="px-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
-            {children}
-        </div>
-    )
-}
-
+type Props = {
+  image: StaticImageData,
+  ctaLabel: string,
+  ctaUrl: string,
+  title: string
+};
+const Card = ({image, ctaLabel, ctaUrl, title}: Props) => (
+  <div className={styles.card}>
+    <div className={styles.card__imageContainer}>
+      <Image fill alt={title} src={image} />
+    </div>
+    <div className={styles.card__textContainer}>
+      <div className={styles.card__textContainer__title}>{title}</div>
+      <ul className={styles.card__textContainer__description}>
+        <li className={styles.card__textContainer__description__item}>Item 1</li>
+        <li className={styles.card__textContainer__description__item}>Item 2</li>
+      </ul>
+      <button
+        className={styles.card__textContainer__cta}
+        onClick={() => window.open(ctaUrl, "_blank", "noopener,noreferrer")}
+      >
+        {ctaLabel}
+      </button>
+    </div>
+  </div>
+)
 export default Card
